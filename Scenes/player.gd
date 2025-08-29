38ca0007937cdb948dc1ff826e3219365b1cd358
Allen Jake Polintan <ajpolintan@dons.usfca.edu>
2025-08-29 10:29:47 -0700

    Changed input settings

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,10 +1,25 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+@onready var camera_mount = $Camera_Mount
+@onready var model = $"3d_Model"
+# Player Variables
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
+# Mouse Sensitivity 
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.5
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# Checks inputs. If there is a mouse input, rotate the player
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		model.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -12,11 +27,11 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * 2
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Forwards", "Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
